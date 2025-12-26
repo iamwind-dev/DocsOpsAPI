e012feb9971face661458673a872063f577bf42f
iamwind-dev <duongdinh3067@gmail.com>
2025-12-27 01:18:24 +0700

    refactor: reorder user ID and status in user status route path

--- src/routes/v2/users.js
+++ src/routes/v2/users.js
@@ -2,8 +2,8 @@ const express = require('express');
 const router = express.Router();
 const userController = require('../../flows/userManagement/user.controller');
 
-// Route: /api/v2/users/status/:userId
-router.get('/status/:userId', (req, res, next) => {
+// Route: /api/v2/users/:userId/status
+router.get('/:userId/status', (req, res, next) => {
     req.params.id = req.params.userId;
     userController.getUserStatus(req, res, next);
 });
